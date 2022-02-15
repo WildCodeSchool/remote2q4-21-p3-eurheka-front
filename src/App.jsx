9888a8ce1f08b6071ce6898397686371a464abb9
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-15 15:23:21 +0100

    creating project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Welcome Heureka!
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
