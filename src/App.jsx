d3b16b727fa7c29b0c8503c1ddb538b0019d75fc
Juliette Mahé <mahe.juliette@outlook.com>
2022-02-15 15:23:17 +0100

    first steps of front

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       Welcome Heureka!
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </ Routes>
     </div>
   );
 }
