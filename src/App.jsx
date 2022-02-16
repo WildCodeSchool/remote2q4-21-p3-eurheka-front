2c759a23cfe43d37f6551f42a9ff5e632849050c
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-16 17:38:18 +0100

    footerv1 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       Welcome Heureka!
       <Routes>
-        <Route path='/' element={<Home />}/>
+        {/* <Route path='/' element={<Home />}/> */}
       </ Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+import Logo_Icone_Jaune from '../../assets/logo/Logo_Icone_Jaune.png'
+
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <div className='logo-container'>
+                <NavLink to="/">
+                    <img className='footer-logo' src={Logo_Icone_Jaune} alt="Eurheka logo" />
+                </ NavLink>
+            </div>
+            <div className='navigation-container'>
+                <ul className='navigation-block'>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Accueil</li>
+                    </ NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Prestations</li>
+                    </NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Bibliothèques</li>
+                    </NavLink>
+                </ul>
+                <ul className='navigation-block'>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Contact / Avis</li>
+                    </NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Connexion</li>
+                    </NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Conditions générales</li>
+                    </NavLink>
+                </ul>
+                <ul className='navigation-block'>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Conditions</li>
+                    </NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>Conditions</li>
+                    </NavLink>
+                    <NavLink to="/">
+                        <li className='navigation-list'>A propos</li>
+                    </NavLink>
+                </ul>
+            </div>
+            <div className='socials'>
+                <div className='icons-container'>
+                    <a href="https://www.google.fr" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-google fa-2xl"></i></a>
+                    <a href="https://www.facebook.fr" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook-f fa-2xl"></i></a>
+                    
+                </div>
+                <div className='icons-container'>
+                    <a href="https://fr.linkedin.com/company/eurheka-estellelorusso" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin-in fa-2xl"></i></a>
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram fa-2xl"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
