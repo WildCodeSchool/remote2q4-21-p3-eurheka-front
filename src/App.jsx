d3d760769a0ae1b3b5a859c0ca4b12277833b903
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-17 15:06:15 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       Welcome Heureka!
       <Routes>
-        <Route path='/' element={<Home />}/>
+        {/* <Route path='/' element={<Home />}/> */}
       </ Routes>
     </div>
   );
