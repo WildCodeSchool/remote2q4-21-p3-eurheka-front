489e9e72ceea4c3b5e72828681ddf32f68c408ba
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-17 17:18:08 +0100

    adding field management ready to send to back

--- src/components/create_user/CreateUser.jsx
+++ src/components/create_user/CreateUser.jsx
@@ -1,7 +1,61 @@
 import React from 'react'
+import {useState} from 'react';
 import './CreateUser.scss';
 
 const CreateUser = () => {
+    const [firstname, setFirstname]=useState('');
+    const [lastname, setLastname]=useState('');
+    const [password1, setPassword1]=useState('');
+    const [password2, setPassword2]=useState('');
+    const [email, setEmail]=useState('');
+
+    const [stage, setStage]=useState(true);
+    const [accompanied, setAccompanied]=useState(false);
+    const [focus, setFocus]=useState(false);
+
+    const submitClick=(e)=>{
+        e.preventDefault();
+        if(firstname&&lastname&&password1&&password2&&email)
+        {
+            if(password1!==password2)
+            {
+                alert('Les mots de passes sont différents');
+                return -1;
+            }
+            if((firstname.length>255)||(lastname.length>255)||(password1.length>255)||(email.length>255))
+            {
+                alert('Les valeurs ne peuvent dépasser 255 caractères');
+                return -1;
+            }
+            const newUser={
+                firstname:firstname,
+                lastname:lastname,
+                password:password1,
+                email:email,
+                stage:stage,
+                focus:focus,
+                accompanied:accompanied
+            }
+            //Send user to back
+            //Go to connexion page ?
+        }
+        else
+        {
+            alert('Veuillez saisir toutes les informations');
+        }
+    }
+    const checkPassord=(newpass)=>{
+        setPassword2(newpass);
+        const pass2=document.getElementById('password2');
+        if(password1!==newpass)
+        {
+            pass2.classList.add('wrongPass');
+        }
+        else{
+            pass2.classList.remove('wrongPass');
+        }
+    }
+
     return (
         <div className='CreateUser'>
             <h2>Créer son profil</h2>
@@ -11,27 +65,27 @@ const CreateUser = () => {
                     <div className="leftFields">
                         <div className="innerLeftFields">
                             <p><label htmlFor="name">Nom<br />
-                                <input className='createUserInput' type="text" id="name" /></label></p>
+                                <input className='createUserInput' type="text" id="name" value={lastname} onChange={(e)=>{setLastname(e.target.value)}}/></label></p>
                             <p><label htmlFor="firstname">Prénom<br />
-                                <input className='createUserInput' type="text" id="firstname" /></label></p>
+                                <input className='createUserInput' type="text" id="firstname" value={firstname} onChange={(e)=>{setFirstname(e.target.value)}}/></label></p>
                         </div>
                         <div className="innerRightFields">
                             <p><label htmlFor="email">Mail<br />
-                                <input className='createUserInput' type="email" id="email" /></label></p>
+                                <input className='createUserInput' type="email" id="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/></label></p>
                             <p><label htmlFor="password1">Mot de passe<br />
-                                <input className='createUserInput' type="password" id="password1" /></label></p>
+                                <input className='createUserInput' type="password" id="password1" value={password1} onChange={(e)=>{setPassword1(e.target.value)}}/></label></p>
                             <p><label htmlFor="password2">Vérifier le mot de passe<br />
-                                <input className='createUserInput' type="password" id="password2" /></label></p>
+                                <input className='createUserInput' type="password" id="password2"  value={password2} onChange={(e)=>{checkPassord(e.target.value)}}/></label></p>
                         </div>
                     </div>
                     <div className="rightFields">
-                        <p><label htmlFor="stage" className='container'><input type="checkbox" className='createUserInput' id="stage" /><span className="checkmark"></span> En recherche d'emploi/stage</label></p>
-                        <p><label htmlFor="accompanied" className='container'><input type="checkbox" className='createUserInput' id="accompanied" /><span className="checkmark"></span>Être accompagné</label></p>
-                        <p><label htmlFor="focus" className='container'><input type="checkbox" className='createUserInput' id="focus" /><span className="checkmark"></span>Faire le point</label></p>
+                        <p><label htmlFor="stage" className='container'><input type="checkbox" className='createUserInput' id="stage" checked={stage} onChange={(e)=>{setStage(e.target.checked)}}/><span className="checkmark"></span> En recherche d'emploi/stage</label></p>
+                        <p><label htmlFor="accompanied" className='container'><input type="checkbox" className='createUserInput' id="accompanied"  checked={accompanied} onChange={(e)=>{setAccompanied(e.target.checked)}} /><span className="checkmark"></span>Être accompagné</label></p>
+                        <p><label htmlFor="focus" className='container'><input type="checkbox" className='createUserInput' id="focus" checked={focus} onChange={(e)=>{setFocus(e.target.checked)}} /><span className="checkmark"></span>Faire le point</label></p>
                     </div>
                 </div>
                 <div className="btnContainer">
-                    <button type="submit" className='btnCreate'>s'inscrire</button>
+                    <button type="submit" className='btnCreate' onClick={(e)=>submitClick(e)}>s'inscrire</button>
                 </div>
             </form>
         </div>
