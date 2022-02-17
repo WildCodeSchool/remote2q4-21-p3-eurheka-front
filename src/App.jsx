54761c878ee423d8b1bdd9ac973b22bbf3c36d5e
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-17 15:30:56 +0100

    routes really added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Services from './pages/services/Services';
+import Library from './pages/library/Library';
+import Contact from './pages/contact/Contact';
 import './App.css';
 
 
@@ -7,8 +11,12 @@ function App() {
     <div className="App">
       Welcome Heureka!
       <Routes>
-        {/* <Route path='/' element={<Home />}/> */}
+        <Route exact path='/' element={<Home />}/>
+        <Route path='/prestations' element={<Services />}/>
+        <Route path='/bibliotheque' element={<Library />}/>
+        <Route path='/contact' element={<Contact />}/>
       </ Routes>
+
     </div>
   );
 }
