7c1c61bdceb05554755adc9dc06dfbf6a3d62e79
Juliette Mahé <mahe.juliette@outlook.com>
2022-02-21 15:13:44 +0100

    begin of log form

---
+++ src/components/log-forms/SignUpForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./SignUpForm.css";
+
+const SignUpForm = () => {
+    return (
+        <div>
+            <p>Inscription</p>
+        </div>
+    )
+}
+
+export default SignUpForm;
