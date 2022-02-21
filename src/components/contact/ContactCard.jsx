740445392650194bb5849020867a5a759741620b
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-21 10:50:49 +0100

    contact v1 + responsive

--- src/components/contact/ContactCard.jsx
+++ src/components/contact/ContactCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
 import "./ContactCard.css"
 
 const ContactCard = () => {
+    const objectRef = useRef(null)
+    const descriptionRef = useRef(null)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const data = {
+                object: objectRef.current.value,
+                description: descriptionRef.current.value
+                }
+        alert("Votre message a bien été envoyé : " + JSON.stringify(data))
+    }
+
     return (
         <div className='ContactCard'>
             <h1 className='contact-title'>CONTACT</h1>
             <h3 className='contact-subtitle'>Une question, une demande ? C'est ici !</h3>
-            <div className='contact-container'>
+            <form onSubmit={handleSubmit} className='contact-container'>
                 <h2 className='contact-form-title'>Votre demande</h2>
                 <label for="object" className='contact-form-subtitle'>Objet</label>
                 <input 
@@ -18,20 +29,22 @@ const ContactCard = () => {
                     placeholder="Prendre un rendez-vous vidéo"
                     onFocus={(e) => e.target.placeholder = ""}  
                     onBlur={(e) => e.target.placeholder = "Prendre un rendez-vous vidéo"}
+                    ref={objectRef}
                 />
                 <label for="description" className='contact-form-subtitle'>Informations supplémentaires</label>
                 <textarea 
                     type="text" 
-                    id="object" 
+                    id="description" 
                     name="description" 
                     className='contact-input-description'
                     placeholder="Je vais passer une entretien pour un post de chargé de communication le 12 février. Ce rendez-vous
                     me premettrait de gagner en confiance avant ce rdv et d’avoir des réponses à mes questions. "
                     onFocus={(e) => e.target.placeholder = ""}  
                     onBlur={(e) => e.target.placeholder = "Je vais passer une entretien pour un post de chargé de communication le 12 février. Ce rendez-vous me premettrait de gagner en confiance avant ce rendez-vous et d’avoir des réponses à mes questions."}
+                    ref={descriptionRef}
                 />
-                <button className='contact-button'>Envoyer</button>
-            </div>    
+                <button type="submit" className='contact-button'>Envoyer</button>
+            </form>    
         </div>
     )
 }
