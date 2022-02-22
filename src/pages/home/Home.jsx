61d1a5fd4a83971b391ee872087b10d496eb80cf
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-22 10:33:16 +0100

    Creating opinion component and style, modify createuser.scss

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import AdviceCardList from "../../components/AdviceCardList";
 import CreateUser from "../../components/create_user/CreateUser";
+import OpinionDiv from "../../components/opiniondiv/OpinionDiv";
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
       <CreateUser />
+      <OpinionDiv />
+      <AdviceCardList />
     </div>
   );
 };
