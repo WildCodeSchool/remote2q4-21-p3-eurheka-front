a189aec6b5a63bbc23df71fe1ed15abd6050a4cd
Juliette Mahé <mahe.juliette@outlook.com>
2022-02-22 10:10:35 +0100

    resolved problem with accueil link in navbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CreateUser from "../../components/create_user/CreateUser";
+import AdviceCardList from "../../components/AdviceCardList";
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
       <CreateUser />
+      <AdviceCardList />
     </div>
   );
 };
