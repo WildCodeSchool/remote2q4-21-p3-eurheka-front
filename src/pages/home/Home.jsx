51182d7e561684bf98a1b66acdf4c6915637a8f6
Mylene Vinci <mylene.vinci@hotmail.com>
2022-02-22 10:56:10 +0100

    pull dev

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import AdviceCardList from '../../components/adviceCardList/AdviceCardList';
 import Partners from '../../components/partners/Partners';
-import CreateUser from "../../components/create_user/CreateUser";
+import CreateUser from '../../components/create_user/CreateUser';
+
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
+      <CreateUser />
       <AdviceCardList />
       <Partners />
-      <CreateUser />
     </div>
   );
 };
