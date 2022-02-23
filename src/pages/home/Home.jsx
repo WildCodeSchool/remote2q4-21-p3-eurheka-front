ee5dcb1635bf76c463578eb7fbed2d5d79a51f7e
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-23 15:38:08 +0100

    useractions container added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import AdviceCardList from '../../components/adviceCardList/AdviceCardList';
 import Partners from '../../components/partners/Partners';
 import CreateUser from "../../components/create_user/CreateUser";
+import UserActionsList from "../../components/user_actions/UserActionsList";
 import './Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
+      <UserActionsList />
       <AdviceCardList />
       <Partners />
       <CreateUser />
