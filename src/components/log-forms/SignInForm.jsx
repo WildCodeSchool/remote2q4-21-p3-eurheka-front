2c9157994fd5c59c4c745f5d29411f37b3623c08
Juliette Mahé <mahe.juliette@outlook.com>
2022-02-23 16:35:15 +0100

    added css changes on btn

--- src/components/log-forms/SignInForm.jsx
+++ src/components/log-forms/SignInForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 import "./SignInForm.css";
 
 const SignInForm = () => {
+    const [email, setEmail]= useState("");
+    const [password, setpassword]= useState("");
+
+    const handleLogin = (e) => {
+
+    }
     return (
         <div>
+            <form action="" onSubmit={handleLogin} id="sign-up-form">
+                <label htmlFor="email">Email</label>
+                <input 
+                type="text" 
+                name="email" 
+                id="email" 
+                onChange={(e) => setEmail(e.target.value)} 
+                value="email" />
+                <input type="submit" value="se connecter" />
+            </form>
             <p>Se connecter</p>
         </div>
     )
