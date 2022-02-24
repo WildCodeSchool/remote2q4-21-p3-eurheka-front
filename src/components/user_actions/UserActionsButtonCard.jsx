d3d965ee871e61a004059b4ba8cbded82d5ecb95
Mylene Vinci <mylene.vinci@hotmail.com>
2022-02-24 17:27:39 +0100

    first step for responsive max-width 390px

--- src/components/user_actions/UserActionsButtonCard.jsx
+++ src/components/user_actions/UserActionsButtonCard.jsx
@@ -4,6 +4,8 @@ import './UserActionsButtonCard.scss'
 const UserActionsButtonCard = () => {
     return (
         <div className='UserActionsButtonCard'>
+            <div className="test">
+
             <h2 className='card-title'>Les offres d'emploi</h2>
             <div className='logo-block'>
                 <i className="fa-solid fa-spinner fa-2xl"></i>
@@ -11,6 +13,7 @@ const UserActionsButtonCard = () => {
             <p className='card-result'>13 offres d’emploi disponibles...</p>
             <p className='card-description'>Trouvez votre futur job parmi nos offres d’emploi disponibles</p>
             <button className='card-button'>Consulter</button>
+            </div>
         </div>
     )
 }
