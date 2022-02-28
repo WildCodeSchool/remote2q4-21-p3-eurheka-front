a0e1a277235c119fbeec6de264e077472646e794
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-28 17:23:57 +0100

    user signIn component fully fonctionnal with errors management

--- src/components/create_user/CreateUser.jsx
+++ src/components/create_user/CreateUser.jsx
@@ -1,84 +1,112 @@
 import React from 'react'
-import {useState} from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 import './CreateUser.scss';
 
 const CreateUser = () => {
-    const [firstname, setFirstname]=useState('');
-    const [lastname, setLastname]=useState('');
-    const [password1, setPassword1]=useState('');
-    const [password2, setPassword2]=useState('');
-    const [email, setEmail]=useState('');
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [password1, setPassword1] = useState('');
+    const [password2, setPassword2] = useState('');
+    const [email, setEmail] = useState('');
 
-    const [stage, setStage]=useState(true);
-    const [accompanied, setAccompanied]=useState(false);
-    const [focus, setFocus]=useState(false);
+    const [stage, setStage] = useState(true);
+    const [accompanied, setAccompanied] = useState(false);
+    const [focus, setFocus] = useState(false);
 
-    const submitClick=(e)=>{
+    const submitClick = (e) => {
         e.preventDefault();
-        if(firstname&&lastname&&password1&&password2&&email)
-        {
-            if(password1!==password2)
-            {
+        //Reset all error panels
+        const errorDivs = document.getElementsByClassName('error');
+        for(let i=0;i<errorDivs.length;i++){
+            errorDivs[i].classList.remove('ErrorDisplay');
+            errorDivs[i].innerHTML='';
+        }
+        if (firstname && lastname && password1 && password2 && email) {
+            if (password1 !== password2) {
                 alert('Les mots de passes sont différents');
                 return -1;
             }
-            if((firstname.length>255)||(lastname.length>255)||(password1.length>255)||(email.length>255))
-            {
+            if ((firstname.length > 255) || (lastname.length > 255) || (password1.length > 255) || (email.length > 255)) {
                 alert('Les valeurs ne peuvent dépasser 255 caractères');
                 return -1;
             }
-            const newUser={
-                firstname:firstname,
-                lastname:lastname,
-                password:password1,
-                email:email,
-                stage:stage,
-                focus:focus,
-                accompanied:accompanied
+            const newUser = {
+                firstname: firstname,
+                lastname: lastname,
+                password: password1,
+                email: email,
+                stage: stage,
+                focus: focus,
+                accompanied: accompanied
             }
             //Send user to back
-            const url=`${process.env.REACT_APP_API_URL}users/`;
-                axios.post(url, newUser)
-              .then(function (response) {
-                console.log(response);
-                if(response.status===201)
-                {
-                    console.log('signup OK');
-                }
-              })
-              .catch(function (error) {
-                const HTTPError=error.response.status;
-                switch(HTTPError)
-                {
-                    case 409 :
+            const url = `${process.env.REACT_APP_API_URL}users/`;
+            axios.post(url, newUser)
+                .then(function (response) {
+                    if (response.status === 201) {
+                        const userId=response.data.userId;
+                    }
+                })
+                .catch(function (error) {
+                    const HTTPError = error.response.status;
+                    let docName = '';
+                    let message = '';
+                    switch (HTTPError) {
+                        case 409:
                             console.log('User already exist');
                             console.log(error.response.data);
+                            docName = 'mailError';
+                            message = "L'adresse mail existe déjà";
+                            let errorDiv = document.getElementById(docName);
+                            errorDiv.innerHTML = message;
+                            errorDiv.classList.add('ErrorDisplay');
                             break;
-                    case 422:
+                        case 422:
                             console.log('Erreur de validation');
                             console.log(error.response.data);
+                            const ErrorArray = error.response.data;
+                            ErrorArray.forEach((error) => {
+                                console.log(error);
+                                if (error.includes('mail')) {
+                                    docName = 'mailError';
+                                    message = "L'adresse mail n'est pas valide";
+                                }
+                                if (error.includes('password')) {
+                                    docName = "passwordError";
+                                    message = "Le mot de passe doit comporter 8 caractères minimum";
+                                }
+                                if (error.includes('firstname')) {
+                                    docName = 'firstNameError';
+                                    message = "Le prénom d'utilisateur n'est pas valide"
+                                }
+                                if (error.includes('lastname')) {
+                                    docName = 'lastNameError';
+                                    message = "Le nom d'utilisateur n'est pas valide"
+                                }
+                                let errorDiv = document.getElementById(docName);
+                                errorDiv.innerHTML = message;
+                                errorDiv.classList.add('ErrorDisplay');
+                            })
                             break;
-                    default : console.log('Unknown error');
+                        default: console.log('Unknown error');
 
-                }
-              });
+                    }
+                });
             //Go to connexion page ?
         }
-        else
-        {
+        else {
             alert('Veuillez saisir toutes les informations');
         }
     }
-    const checkPassord=(newpass)=>{
+    const checkPassord = (newpass) => {
         setPassword2(newpass);
-        const pass2=document.getElementById('password2');
-        if(password1!==newpass)
-        {
+        const pass2 = document.getElementById('password2');
+        if (password1 !== newpass) {
             pass2.classList.add('wrongPass');
         }
-        else{
+        else {
             pass2.classList.remove('wrongPass');
         }
     }
@@ -91,28 +119,39 @@ const CreateUser = () => {
                 <div className="globalContainer">
                     <div className="leftFields">
                         <div className="innerLeftFields">
-                           <label htmlFor="name" className='labelLeft'>Nom<br />
-                                <input className='createUserInput' type="text" id="name" value={lastname} onChange={(e)=>{setLastname(e.target.value)}}/></label>
+                            <label htmlFor="name" className='labelLeft'>Nom<br />
+                                <input className='createUserInput' type="text" id="name" value={lastname} onChange={(e) => { setLastname(e.target.value) }} />
+                                <div className='error' id="lastNameError">
+                                </div>
+                            </label>
                             <label htmlFor="firstname" className='labelLeft'>Prénom<br />
-                                <input className='createUserInput' type="text" id="firstname" value={firstname} onChange={(e)=>{setFirstname(e.target.value)}}/></label>
+                                <input className='createUserInput' type="text" id="firstname" value={firstname} onChange={(e) => { setFirstname(e.target.value) }} />
+                                <div className='error' id="firstNameError">
+                                </div>
+                            </label>
                         </div>
                         <div className="innerRightFields">
-                           <label htmlFor="email" className='labelLeft'>Mail<br />
-                                <input className='createUserInput' type="email" id="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/></label>
+                            <label htmlFor="email" className='labelLeft'>Mail<br />
+                                <input className='createUserInput' type="email" id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                                <div className='error' id="mailError">
+                                </div>
+                            </label>
                             <label htmlFor="password1" className='labelLeft'>Mot de passe<br />
-                                <input className='createUserInput' type="password" id="password1" value={password1} onChange={(e)=>{setPassword1(e.target.value)}}/></label>
+                                <input className='createUserInput' type="password" id="password1" value={password1} onChange={(e) => { setPassword1(e.target.value) }} />
+                                <div className='error' id="passwordError">
+                                </div></label>
                             <label htmlFor="password2" className='labelLeft'>Vérifier le mot de passe<br />
-                                <input className='createUserInput' type="password" id="password2"  value={password2} onChange={(e)=>{checkPassord(e.target.value)}}/></label>
+                                <input className='createUserInput' type="password" id="password2" value={password2} onChange={(e) => { checkPassord(e.target.value) }} /></label>
                         </div>
                     </div>
                     <div className="rightFields">
-                        <label htmlFor="stage" className='container'><input type="checkbox" className='createUserInput' id="stage" checked={stage} onChange={(e)=>{setStage(e.target.checked)}}/><span className="checkmark"></span> En recherche d'emploi/stage</label>
-                        <label htmlFor="accompanied" className='container'><input type="checkbox" className='createUserInput' id="accompanied"  checked={accompanied} onChange={(e)=>{setAccompanied(e.target.checked)}} /><span className="checkmark"></span>Être accompagné</label>
-                        <label htmlFor="focus" className='container'><input type="checkbox" className='createUserInput' id="focus" checked={focus} onChange={(e)=>{setFocus(e.target.checked)}} /><span className="checkmark"></span>Faire le point</label>
+                        <label htmlFor="stage" className='container'><input type="checkbox" className='createUserInput' id="stage" checked={stage} onChange={(e) => { setStage(e.target.checked) }} /><span className="checkmark"></span> En recherche d'emploi/stage</label>
+                        <label htmlFor="accompanied" className='container'><input type="checkbox" className='createUserInput' id="accompanied" checked={accompanied} onChange={(e) => { setAccompanied(e.target.checked) }} /><span className="checkmark"></span>Être accompagné</label>
+                        <label htmlFor="focus" className='container'><input type="checkbox" className='createUserInput' id="focus" checked={focus} onChange={(e) => { setFocus(e.target.checked) }} /><span className="checkmark"></span>Faire le point</label>
                     </div>
                 </div>
                 <div className="btnContainer">
-                    <button type="submit" className='btnCreate' onClick={(e)=>submitClick(e)}>s'inscrire</button>
+                    <button type="submit" className='btnCreate' onClick={(e) => submitClick(e)}>s'inscrire</button>
                 </div>
             </form>
         </div>
