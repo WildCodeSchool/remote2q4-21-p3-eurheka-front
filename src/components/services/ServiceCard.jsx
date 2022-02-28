96edadf0882bb0fd4542b6db00be5578ba81d0ac
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-28 17:36:58 +0100

    mapping and generate cards

--- src/components/services/ServiceCard.jsx
+++ src/components/services/ServiceCard.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import './ServiceCard.scss'
 
-export const ServiceCard = () => {
+export const ServiceCard = (props) => {
+    // const {containerFormat, setContainerFormat} = props
+
     return (
         <div className='ServiceCard'>
-            <h2 className='serviceCardTitle'>Réussir son embauche</h2>
-            <h3 className='serviceCardSubTitle'>Comment bien préparer et réussir son embauche ?</h3>
-            <ul className='serviceDescriptionListOn'>
-                <li>Se renseigner</li>
-                <li>Se présenter</li>
-                <li>Se renseigner</li>
-                <li>Se présenter</li>
-                <li>Se renseigner</li>
-                <li>Se présenter</li>
-                <li>Se renseigner</li>
-                <li>Se présenter</li>
-            </ul>
-            <button className='serviceDescriptionListOn'>bouton</button>
+            <div className={props.containerClass}>
+                <h2 className='serviceCardTitle'>{props.title}</h2>
+                <h3 className='serviceCardSubTitle'>{props.subTitle}</h3>
+                <ul className='serviceDescriptionListOn'>
+                    {props.task1 ? <li>{props.task1}</li> : null }
+                    {props.task2 ? <li>{props.task2}</li> : null }
+                    {props.task3 ? <li>{props.task3}</li> : null }
+                    {props.task4 ? <li>{props.task4}</li> : null }
+                    {props.task5 ? <li>{props.task5}</li> : null }
+                    {props.task6 ? <li>{props.task6}</li> : null }
+                    {props.task7 ? <li>{props.task7}</li> : null }
+                    {props.task8 ? <li>{props.task8}</li> : null }
+                </ul>
+                <button className='serviceDescriptionListOn'>bouton</button>
+            </div>
         </div>
     )
 }
--- src/components/services/ServicesList.jsx
+++ src/components/services/ServicesList.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import { ServiceCard } from './ServiceCard'
-import ServiceCardRedux from './ServiceCardRedux'
-import './ServicesList.scss'
+import React, { useState } from 'react';
+import { ServiceCard } from './ServiceCard';
+import ServiceCardRedux from './ServiceCardRedux';
+import themeServices from '../../assets/data/themeServices';
+import './ServicesList.scss';
 
 const ServicesList = () => {
+    // const [containerFormat, setContainerFormat] = useState(false)
+
     return (
         <div className='ServicesList'>
-            <div className='columnList'>
-                <ServiceCard />
+            {themeServices.map((data) => (
+                <ServiceCard containerClass={(data.id % 2 ===0) ? 'lightContainer' : 'darkContainer'}
+                    key={data.id}
+                    title={data.title}
+                    subTitle={data.subTitle}
+                    task1={data.task1}
+                    task2={data.task2}
+                    task3={data.task3}
+                    task4={data.task4}
+                    task5={data.task5}
+                    task6={data.task6}
+                    task7={data.task7}
+                    task8={data.task8}
+                />
+            ))
+
+            }
+            {/* <div className='columnList'>
+                <ServiceCard containerFormat={containerFormat} setContainerFormat={setContainerFormat}/>
                 <ServiceCardRedux />
             </div>
             <div className='columnList'>
@@ -17,7 +37,7 @@ const ServicesList = () => {
             <div className='columnList'>
                 <ServiceCard />
                 <ServiceCardRedux />
-            </div>
+            </div> */}
         </div>
     )
 }
