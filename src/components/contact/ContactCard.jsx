90f62ee7a6de04657ef3dbf1f104a8171453a0bd
Florent-Lefevre <lefevre.flo@gmail.com>
2022-02-28 14:40:25 +0100

    add firstname lastname

--- src/components/contact/ContactCard.jsx
+++ src/components/contact/ContactCard.jsx
@@ -26,50 +26,56 @@ const ContactCard = () => {
             <h3 className='contact-subtitle'>Une question, une demande ? C'est ici !</h3>
             <form onSubmit={handleSubmit} className='contact-container'>
                 <h2 className='contact-form-title'>Votre demande</h2>
-                <label for="firstname" className='contact-form-subtitle'>Prénom</label>
-                <input 
-                    type="text" 
-                    id="firstname" 
-                    name="firstname" 
-                    className='contact-input-object' 
-                    placeholder="edouard"
-                    onFocus={(e) => e.target.placeholder = ""}  
-                    onBlur={(e) => e.target.placeholder = "edouard"}
-                    ref={firstNameRef}
-                />
-                <label for="lastname" className='contact-form-subtitle'>Nom</label>
-                <input 
-                    type="text" 
-                    id="lastname" 
-                    name="lastname" 
-                    className='contact-input-object' 
-                    placeholder="doe"
-                    onFocus={(e) => e.target.placeholder = ""}  
-                    onBlur={(e) => e.target.placeholder = "doe"}
-                    ref={lastNameRef}
-                />
-                <label for="email" className='contact-form-subtitle'>Email</label>
-                <input 
-                    type="text" 
-                    id="email" 
-                    name="email" 
-                    className='contact-input-object' 
-                    placeholder="edouard.doe@gmail.com"
-                    onFocus={(e) => e.target.placeholder = ""}  
-                    onBlur={(e) => e.target.placeholder = "edouard.doe@gmail.com"}
-                    ref={emailRef}
-                />
-                <label for="object" className='contact-form-subtitle'>Objet</label>
-                <input 
-                    type="text" 
-                    id="object" 
-                    name="object" 
-                    className='contact-input-object' 
-                    placeholder="Prendre un rendez-vous vidéo"
-                    onFocus={(e) => e.target.placeholder = ""}  
-                    onBlur={(e) => e.target.placeholder = "Prendre un rendez-vous vidéo"}
-                    ref={objectRef}
-                />
+                <div className='contact-information-container'>
+                    <div className='contact-input-container'>
+                        <label for="firstname" className='contact-form-subtitle'>Prénom</label>
+                        <input 
+                            type="text" 
+                            id="firstname" 
+                            name="firstname" 
+                            className='contact-input' 
+                            placeholder="edouard"
+                            onFocus={(e) => e.target.placeholder = ""}  
+                            onBlur={(e) => e.target.placeholder = "edouard"}
+                            ref={firstNameRef}
+                        />
+                        <label for="lastname" className='contact-form-subtitle'>Nom</label>
+                        <input 
+                            type="text" 
+                            id="lastname" 
+                            name="lastname" 
+                            className='contact-input' 
+                            placeholder="doe"
+                            onFocus={(e) => e.target.placeholder = ""}  
+                            onBlur={(e) => e.target.placeholder = "doe"}
+                            ref={lastNameRef}
+                        />
+                    </div>
+                    <div className='contact-input-container'>
+                    <label for="email" className='contact-form-subtitle'>Email</label>
+                        <input 
+                            type="text" 
+                            id="email" 
+                            name="email" 
+                            className='contact-input' 
+                            placeholder="edouard.doe@gmail.com"
+                            onFocus={(e) => e.target.placeholder = ""}  
+                            onBlur={(e) => e.target.placeholder = "edouard.doe@gmail.com"}
+                            ref={emailRef}
+                        />
+                        <label for="object" className='contact-form-subtitle'>Objet</label>
+                        <input 
+                            type="text" 
+                            id="object" 
+                            name="object" 
+                            className='contact-input' 
+                            placeholder="Prendre un rendez-vous vidéo"
+                            onFocus={(e) => e.target.placeholder = ""}  
+                            onBlur={(e) => e.target.placeholder = "Prendre un rendez-vous vidéo"}
+                            ref={objectRef}
+                        />
+                    </div>
+                </div>
                 <label for="description" className='contact-form-subtitle'>Informations supplémentaires</label>
                 <textarea 
                     type="text" 
