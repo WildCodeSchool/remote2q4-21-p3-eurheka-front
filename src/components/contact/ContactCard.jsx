01e1b0f90baf5d052b9dcf86357f7b930fab3cbe
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-04 16:01:54 +0100

    pull

--- src/components/contact/ContactCard.jsx
+++ src/components/contact/ContactCard.jsx
@@ -1,23 +1,56 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import "./ContactCard.css"
 
 const ContactCard = () => {
-    const firstNameRef = useRef(null)
-    const lastNameRef = useRef(null)
-    const emailRef = useRef(null)
-    const objectRef = useRef(null)
-    const descriptionRef = useRef(null)
+    const [firstname, setFirstname] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [description, setDescription] = useState('');
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const data = {
-                firstName: firstNameRef.current.value,
-                lastName: lastNameRef.current.value,
-                email: emailRef.current.value,
-                object: objectRef.current.value,
-                description: descriptionRef.current.value
-                }
-        alert("Votre message a bien été envoyé : " + JSON.stringify(data))
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let errorMessage = document.getElementsByClassName("Error");
+        for (let i=0; i<errorMessage.length; i++){
+            errorMessage[i].innerHTML="";
+        }
+        const newContactMessage = {
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                subject: subject,
+                description: description
+        }
+        
+        const url = `${process.env.REACT_APP_API_URL}mail/`;
+            axios.post(url, newContactMessage)
+                .then(function (response){
+                    if (response.status === 200) {
+                        alert("Votre message a bien été envoyé")
+                        setFirstname("");
+                        setLastname("");
+                        setEmail("");
+                        setSubject("");
+                        setDescription("");
+                    }
+                })
+                .catch(function (error){
+                    const HTTPError = error.response.status;
+                    let docName = '';
+                    let message = '';
+                    if (HTTPError === 500) {
+                        console.log('SMTP server not found');
+                        console.log(error.response.data);
+                        docName = 'emailError';
+                        message = "Une erreur s'est produite lors de l'envoi";
+                        let errorDiv = document.getElementById(docName);
+                        errorDiv.innerHTML = message;
+                        errorDiv.classList.add('ErrorDisplay');
+                    }  else {
+                        console.log('Unknown error');
+                    }
+                })
     }
 
     return (
@@ -37,7 +70,7 @@ const ContactCard = () => {
                             placeholder="edouard"
                             onFocus={(e) => e.target.placeholder = ""}  
                             onBlur={(e) => e.target.placeholder = "edouard"}
-                            ref={firstNameRef}
+                            onChange={(e) => setFirstname(e.target.value)}
                         />
                         <label for="lastname" className='contact-form-subtitle'>Nom</label>
                         <input 
@@ -48,7 +81,7 @@ const ContactCard = () => {
                             placeholder="doe"
                             onFocus={(e) => e.target.placeholder = ""}  
                             onBlur={(e) => e.target.placeholder = "doe"}
-                            ref={lastNameRef}
+                            onChange={(e) => setLastname(e.target.value)}
                         />
                     </div>
                     <div className='contact-input-container'>
@@ -61,18 +94,18 @@ const ContactCard = () => {
                             placeholder="edouard.doe@gmail.com"
                             onFocus={(e) => e.target.placeholder = ""}  
                             onBlur={(e) => e.target.placeholder = "edouard.doe@gmail.com"}
-                            ref={emailRef}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
-                        <label for="object" className='contact-form-subtitle'>Objet</label>
+                        <label for="subject" className='contact-form-subtitle'>Objet</label>
                         <input 
                             type="text" 
-                            id="object" 
-                            name="object" 
+                            id="subject" 
+                            name="subject" 
                             className='contact-input' 
                             placeholder="Prendre un rendez-vous vidéo"
                             onFocus={(e) => e.target.placeholder = ""}  
                             onBlur={(e) => e.target.placeholder = "Prendre un rendez-vous vidéo"}
-                            ref={objectRef}
+                            onChange={(e) => setSubject(e.target.value)}
                         />
                     </div>
                 </div>
@@ -86,7 +119,7 @@ const ContactCard = () => {
                     me premettrait de gagner en confiance avant ce rdv et d’avoir des réponses à mes questions. "
                     onFocus={(e) => e.target.placeholder = ""}  
                     onBlur={(e) => e.target.placeholder = "Je vais passer une entretien pour un post de chargé de communication le 12 février. Ce rendez-vous me premettrait de gagner en confiance avant ce rendez-vous et d’avoir des réponses à mes questions."}
-                    ref={descriptionRef}
+                    onChange={(e) => setDescription(e.target.value)}
                 />
                 <button type="submit" className='contact-button'>Envoyer</button>
             </form>    
