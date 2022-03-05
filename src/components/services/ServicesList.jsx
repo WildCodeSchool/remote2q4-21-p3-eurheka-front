49f85c283c9956347d4c32cb156b5e7cbd1e9087
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-05 19:13:39 +0100

    servicelist optimisation

--- src/components/services/ServicesList.jsx
+++ src/components/services/ServicesList.jsx
@@ -4,93 +4,46 @@ import themeServices from '../../assets/data/themeServices';
 import './ServicesList.scss';
 
 const ServicesList = () => {
+    console.log(themeServices[0+1].id)
+    const cardRender = [];
+    for (let i = 0; i < themeServices.length; i++) {
+        cardRender.push(
+            <ServiceCard 
+                    containerClass={i === 0 || i === 4 ? 'firstCard' : 'secondCardReduce'}
+                    containerClassReduce={i === 0 || i === 4 ? 'firstCardReduce' : 'firstCard'}
+                    secondContainerClass={i === 0 || i === 4 ? 'secondCard' : 'firstCardReduce'}
+                    SecondContainerClassReduce={i === 0 || i === 4 ? 'secondCardReduce' : 'secondCard'}
+                    key={themeServices[i].id}
+                    title={themeServices[i].title}
+                    subTitle={themeServices[i].subTitle}
+                    task1={themeServices[i].task1}
+                    task2={themeServices[i].task2}
+                    task3={themeServices[i].task3}
+                    task4={themeServices[i].task4}
+                    task5={themeServices[i].task5}
+                    task6={themeServices[i].task6}
+                    task7={themeServices[i].task7}
+                    task8={themeServices[i].task8}
+                    keyb={themeServices[i+1].id}
+                    titleb={themeServices[i+1].title}
+                    subTitleb={themeServices[i+1].subTitle}
+                    task1b={themeServices[i+1].task1}
+                    task2b={themeServices[i+1].task2}
+                    task3b={themeServices[i+1].task3}
+                    task4b={themeServices[i+1].task4}
+                    task5b={themeServices[i+1].task5}
+                    task6b={themeServices[i+1].task6}
+                    task7b={themeServices[i+1].task7}
+                    task8b={themeServices[i+1].task8}
+                />)
+            i = i +1
+            
+        }
+    
 
     return (
         <div className='ServicesList'>
-            <ServiceCard 
-                    containerClass={'firstCard'}
-                    containerClassReduce={'firstCardReduce'}
-                    secondContainerClass={'secondCard'}
-                    SecondContainerClassReduce={'secondCardReduce'}
-                    key={themeServices[0].id}
-                    title={themeServices[0].title}
-                    subTitle={themeServices[0].subTitle}
-                    task1={themeServices[0].task1}
-                    task2={themeServices[0].task2}
-                    task3={themeServices[0].task3}
-                    task4={themeServices[0].task4}
-                    task5={themeServices[0].task5}
-                    task6={themeServices[0].task6}
-                    task7={themeServices[0].task7}
-                    task8={themeServices[0].task8}
-                    keyb={themeServices[1].id}
-                    titleb={themeServices[1].title}
-                    subTitleb={themeServices[1].subTitle}
-                    task1b={themeServices[1].task1}
-                    task2b={themeServices[1].task2}
-                    task3b={themeServices[1].task3}
-                    task4b={themeServices[1].task4}
-                    task5b={themeServices[1].task5}
-                    task6b={themeServices[1].task6}
-                    task7b={themeServices[1].task7}
-                    task8b={themeServices[1].task8}
-                />
-            <ServiceCard 
-                    containerClass={'firstCardReduce'}
-                    containerClassReduce={'firstCard'}
-                    secondContainerClass={'secondCardReduce'}
-                    SecondContainerClassReduce={'secondCard'}
-                    key={themeServices[2].id}
-                    title={themeServices[2].title}
-                    subTitle={themeServices[2].subTitle}
-                    task1={themeServices[2].task1}
-                    task2={themeServices[2].task2}
-                    task3={themeServices[2].task3}
-                    task4={themeServices[2].task4}
-                    task5={themeServices[2].task5}
-                    task6={themeServices[2].task6}
-                    task7={themeServices[2].task7}
-                    task8={themeServices[2].task8}
-                    keyb={themeServices[3].id}
-                    titleb={themeServices[3].title}
-                    subTitleb={themeServices[3].subTitle}
-                    task1b={themeServices[3].task1}
-                    task2b={themeServices[3].task2}
-                    task3b={themeServices[3].task3}
-                    task4b={themeServices[3].task4}
-                    task5b={themeServices[3].task5}
-                    task6b={themeServices[3].task6}
-                    task7b={themeServices[3].task7}
-                    task8b={themeServices[3].task8}
-                />
-            <ServiceCard 
-                containerClass={'firstCard'}
-                containerClassReduce={'firstCardReduce'}
-                secondContainerClass={'secondCard'}
-                SecondContainerClassReduce={'secondCardReduce'}
-                    key={themeServices[4].id}
-                    title={themeServices[4].title}
-                    subTitle={themeServices[4].subTitle}
-                    task1={themeServices[4].task1}
-                    task2={themeServices[4].task2}
-                    task3={themeServices[4].task3}
-                    task4={themeServices[4].task4}
-                    task5={themeServices[4].task5}
-                    task6={themeServices[4].task6}
-                    task7={themeServices[4].task7}
-                    task8={themeServices[4].task8}
-                    keyb={themeServices[5].id}
-                    titleb={themeServices[5].title}
-                    subTitleb={themeServices[5].subTitle}
-                    task1b={themeServices[5].task1}
-                    task2b={themeServices[5].task2}
-                    task3b={themeServices[5].task3}
-                    task4b={themeServices[5].task4}
-                    task5b={themeServices[5].task5}
-                    task6b={themeServices[5].task6}
-                    task7b={themeServices[5].task7}
-                    task8b={themeServices[5].task8}
-                />
+            {cardRender}
         </div>
     )
 }
