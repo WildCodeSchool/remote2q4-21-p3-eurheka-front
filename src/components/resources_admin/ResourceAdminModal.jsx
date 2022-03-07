f2b7586ab5c9c605fcd99b99f034afd92da28eae
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-07 11:04:12 +0100

    adding array for test, modify useEffect for async

--- src/components/resources_admin/ResourceAdminModal.jsx
+++ src/components/resources_admin/ResourceAdminModal.jsx
@@ -1,32 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './ResourceAdminModal.scss';
-
+const resArrayTheme = [
+    {
+        id: 1,
+        name: 'RH'
+    },
+    {
+        id: 2,
+        name: 'Test TH'
+    },
+    {
+        id: 3,
+        name: 'Test 2 THEME'
+    },
+    {
+        id: 4,
+        name: 'Test 3 THEME'
+    }
+];
 const ResourceAdminModal = ({ resource, displayModal }) => {
     const [docs, setDocs] = useState(null);
+    const [themes, setThemes] = useState([]);
+    const [themeUsed, setThemeUsed] = useState([]);
+    const [visibility, setVisibility] = useState();
     const url = `${process.env.REACT_APP_API_URL}resource/admin/` + resource;
 
-    useEffect(async () => {
-        if (resource !== null) {
-            axios.get(url, { withCredentials: true })
-                .then((res) => res.data)
-                .then((data) => {
-                    setDocs(data)
-                })
-                .catch((err) => {
-                    console.log(err);
-                    const HTTPError = err.response.status;
-                    if (HTTPError === 401) {
-                        alert('Vous avez été déconnecté.');
-                        window.location = '/';
-                    }
-                })
+    useEffect(() => {
+        const fetchDoc = async () => {
+            if (resource !== null) {
+                await axios.get(url, { withCredentials: true })
+                    .then((res) => res.data)
+                    .then((data) => {
+                        setDocs(data);
+                        setVisibility(data.visibility);
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        const HTTPError = err.response.status;
+                        if (HTTPError === 401) {
+                            alert('Vous avez été déconnecté.');
+                            window.location = '/';
+                        }
+                    })
+            }
         }
+        fetchDoc();
     }, [resource]);
 
-    useEffect(async () => {
+    useEffect(() => {
         //axios theme
-    }, [])
+        const fetchTheme = async () => {
+            //axios
+        }
+        setThemes(resArrayTheme);
+    }, []);
+    const handleChangeTheme = () => {
+
+    }
+    //Make
+    resArrayTheme.forEach((themeItem) => {
+        //if()
+    });
 
     return (
         <div id="resourceModal" className='ResourceModal'>
@@ -34,7 +69,7 @@ const ResourceAdminModal = ({ resource, displayModal }) => {
                 Nom : {docs.name}<br />
                 Chemin {docs.path}<br />
                 catégorie : {docs.CategoryResource}<br />
-                Visibilité : <select value={docs.visibility}>
+                Visibilité : <select value={visibility}>
                     <option value="1">Utilisateur non connecté</option>
                     <option value="2">Utilisateur connecté</option>
                     <option value="3">Entreprises</option>
