cd085f643838f552663da993cbff1260db3f37b8
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-14 10:27:59 +0100

    adding user admin component

---
+++ src/components/usermanagement/UserAdminContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './UserAdminContainer.scss';
+
+const UserAdminContainer = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UserAdminContainer
