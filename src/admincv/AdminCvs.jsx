b04a4e7d397433d26c2007ccad1ba9d7c0e07b90
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-24 19:29:50 +0100

    adding display CV in admin, have to check link

--- src/admincv/AdminCvs.jsx
+++ src/admincv/AdminCvs.jsx
@@ -1,29 +1,29 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './AdminCvs.scss';
 
 const AdminCvs = () => {
     const [showComponent, setShowComponent] = useState(false);
-    const [cvs,setCvs]=useState([]);
+    const [cvs, setCvs] = useState([]);
 
-    useEffect(()=>{
-        const getCvs=async()=>{
-            const url = `${process.env.REACT_APP_API_URL}event/category/`;
+    useEffect(() => {
+        const getCvs = async () => {
+            const url = `${process.env.REACT_APP_API_URL}cv/admin/`;
             await axios
-                .get(url,{withCredentials:true})
-                .then((result)=>{
+                .get(url, { withCredentials: true })
+                .then((result) => {
                     setCvs(result.data);
                 })
-                .catch((err)=>{
+                .catch((err) => {
                     console.log(err);
-                    if(err.status===401){
+                    if (err.status === 401) {
                         alert('Vous avez été déconnecté.');
                         window.location = '/';
                     }
                 })
         }
-
-    },[]);
+        getCvs();
+    }, []);
 
     const handleShowClick = (id) => {
         const docContainer = document.getElementById(id);
@@ -39,12 +39,20 @@ const AdminCvs = () => {
     return (
         <div className='AdminCvs'>
             <h2 className='CvsAdminTitle'>Consulter les Cvs en ligne</h2>
-                <i className={showComponent ? "fa-solid fa-chevron-up CloseFolding" : "fa-solid fa-chevron-down CloseFolding"} onClick={() => handleShowClick('CvsAdminBloc')}></i>
-                <div className="CvsAdminBloc BlocHidden" id="CvsAdminBloc">
-                    <div className="CvsAdminContainer">
+            <i className={showComponent ? "fa-solid fa-chevron-up CloseFolding" : "fa-solid fa-chevron-down CloseFolding"} onClick={() => handleShowClick('CvsAdminBloc')}></i>
+            <div className="CvsAdminBloc BlocHidden" id="CvsAdminBloc">
+                <div className="CvsAdminContainer">
+                    <ul className='CvsAdminList'>
+                        {cvs&&cvs.map((cv)=>{
+                            return(
+                                <li key={cv.id_cv} className="CvsAdminItem"><a href={cv.path} target="_blank">CV de {cv.user}</a></li>
+                            )
+                        })
 
-                        </div>
+                        }
+                    </ul>
                 </div>
+            </div>
         </div>
     )
 }
