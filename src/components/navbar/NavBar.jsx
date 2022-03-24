0cc709516bd42703f384ee900fdda4ff4e00e23f
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-24 15:01:55 +0100

    umont when connected and scroll to menu

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -90,22 +90,22 @@ const NavBar = () => {
             <div className='right-container'>
                 {!connected&& 
                     <div className='login-button'>
-                        <NavLink to="/login" className='login-link'>Se connecter</NavLink>
+                        <NavLink to="/login" className='login-link' onClick={scrollToMenu}>Se connecter</NavLink>
                     </div>
                 }
                 {connected&&
                     <div className='connected-container'>
                         {admin&&
                             <NavLink to="/admin" className={({ isActive }) =>
-                            isActive ? "selected" : "navigation-link"} >Administration</NavLink>
+                            isActive ? "selected" : "navigation-link"} onClick={scrollToMenu}>Administration</NavLink>
                         }
                         {!admin&&independantUser&&
                             <NavLink to="/mon-profil-particulier" className={({ isActive }) =>
-                            isActive ? "selected" : "navigation-link"} >Mon profil</NavLink>
+                            isActive ? "selected" : "navigation-link"} onClick={scrollToMenu}>Mon profil</NavLink>
                         }
                         {!admin&&companyUser&&
                             <NavLink to="/mon-profil-entreprise" className={({ isActive }) =>
-                            isActive ? "selected" : "navigation-link"}>Mon profil entreprise</NavLink>
+                            isActive ? "selected" : "navigation-link" } onClick={scrollToMenu}>Mon profil entreprise</NavLink>
                         }
                         <Disconnect />
                     </div>
