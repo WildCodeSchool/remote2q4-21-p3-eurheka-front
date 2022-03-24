e2a0a7731e05b2f251fd7ba0126a2d246f74d039
Mylene Vinci <mylene.vinci@hotmail.com>
2022-03-24 12:01:36 +0100

    add link on advicecard

--- src/components/adviceCard/AdviceCard.jsx
+++ src/components/adviceCard/AdviceCard.jsx
@@ -4,16 +4,18 @@ import './AdviceCard.css';
 const AdviceCard = (props) => {
     return (
         <div className='AdviceCard '>
-            <div className={props.className} >
-                <h3 className='h3-title'>{props.title}</h3>
-                <ul className='taskList'>
-                    {props.task1 ? <li>{props.task1}</li> : null }
-                    {props.task2 ? <li>{props.task2}</li> : null }
-                    {props.task3 ? <li>{props.task3}</li> : null }
-                    {props.task4 ? <li>{props.task4}</li> : null }
-                </ul>
-                <p className='dots-paragraph'>{props.task5}</p>
-            </div>
+            <a className='adviceCardLink' href={props.link}>
+                <div className={props.className} >
+                    <h3 className='h3-title'>{props.title}</h3>
+                    <ul className='taskList'>
+                        {props.task1 ? <li>{props.task1}</li> : null }
+                        {props.task2 ? <li>{props.task2}</li> : null }
+                        {props.task3 ? <li>{props.task3}</li> : null }
+                        {props.task4 ? <li>{props.task4}</li> : null }
+                    </ul>
+                    <p className='dots-paragraph'>{props.task5}</p>
+                </div>
+                </a>
         </div>
     )
 }
