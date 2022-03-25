311608fdf91c2c49c198a08bb178e1e7c686bf8b
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-25 18:21:16 +0100

    deleting job offer OK

--- src/components/adminjobs/AdminJobContainer.jsx
+++ src/components/adminjobs/AdminJobContainer.jsx
@@ -1,13 +1,13 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import './AdminJobContainer.scss';
 
-const AdminJobContainer = ({reload, setReload}) => {
-    const [jobList,setJobList]=useState([]);
+const AdminJobContainer = ({ reload, setReload }) => {
+    const [jobList, setJobList] = useState([]);
 
-    useEffect(()=>{
-        const getOffers=async()=>{
+    useEffect(() => {
+        const getOffers = async () => {
             const url = `${process.env.REACT_APP_API_URL}job/admin/`;
             await axios
                 .get(url, { withCredentials: true })
@@ -25,27 +25,44 @@ const AdminJobContainer = ({reload, setReload}) => {
                 });
         }
         getOffers();
-    },[reload]);
+    }, [reload]);
 
-    const handleDeleteJob=(id)=>{
-
+    const handleDeleteJob = (id) => {
+        if (window.confirm("Voulez vous supprimer cette offre ?")) {
+            const url = `${process.env.REACT_APP_API_URL}job/${id}`;
+            axios
+                .delete(url, { withCredentials: true })
+                .then((result) => {
+                    if (result.status === 204) {
+                        setReload(!reload);
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    if (err.status === 401) {
+                        alert('Vous avez été déconnecté.');
+                        window.location = '/';
+                    }
+                });
+        }
     }
 
     return (
         <div className='AdminJobContainer'>
             <h3>Liste des offres d'emploi présentes</h3>
             <ul className="AdminJobList">
-                {jobList&&jobList.map((jobItem)=>{
-                    return(
-                    <li key={jobItem.id_job}
-                        className="AdminJobItem">
+                {jobList && jobList.map((jobItem) => {
+                    return (
+                        <li key={jobItem.id_job}
+                            className="AdminJobItem">
                             <a href={`${process.env.REACT_APP_URL}${jobItem.path}`} target="_blank">{jobItem.name}</a> - {jobItem.name_offer}
                             <input
-                                 type="button" 
-                                 value="Supprimer" 
-                                 onClick={()=>handleDeleteJob(jobItem.id_job)}
-                                 className="BtnDeleteJob"/></li>
-                 ) })}
+                                type="button"
+                                value="Supprimer"
+                                onClick={() => handleDeleteJob(jobItem.id_job)}
+                                className="BtnDeleteJob" /></li>
+                    )
+                })}
             </ul>
         </div>
     )
