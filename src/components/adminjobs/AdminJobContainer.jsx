780022703544ea585d7fc145d75cc5a4ee7bf3b9
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-03-25 18:10:51 +0100

    display list of job OK

--- src/components/adminjobs/AdminJobContainer.jsx
+++ src/components/adminjobs/AdminJobContainer.jsx
@@ -1,20 +1,52 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
+import './AdminJobContainer.scss';
 
 const AdminJobContainer = ({reload, setReload}) => {
     const [jobList,setJobList]=useState([]);
 
     useEffect(()=>{
         const getOffers=async()=>{
-
+            const url = `${process.env.REACT_APP_API_URL}job/admin/`;
+            await axios
+                .get(url, { withCredentials: true })
+                .then((result) => {
+                    if (result.status === 200) {
+                        setJobList(result.data)
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    if (err.status === 401) {
+                        alert('Vous avez été déconnecté.');
+                        window.location = '/';
+                    }
+                });
         }
         getOffers();
     },[reload]);
 
+    const handleDeleteJob=(id)=>{
+
+    }
+
     return (
-        <div>
-            
+        <div className='AdminJobContainer'>
+            <h3>Liste des offres d'emploi présentes</h3>
+            <ul className="AdminJobList">
+                {jobList&&jobList.map((jobItem)=>{
+                    return(
+                    <li key={jobItem.id_job}
+                        className="AdminJobItem">
+                            <a href={`${process.env.REACT_APP_URL}${jobItem.path}`} target="_blank">{jobItem.name}</a> - {jobItem.name_offer}
+                            <input
+                                 type="button" 
+                                 value="Supprimer" 
+                                 onClick={()=>handleDeleteJob(jobItem.id_job)}
+                                 className="BtnDeleteJob"/></li>
+                 ) })}
+            </ul>
         </div>
     )
 }
