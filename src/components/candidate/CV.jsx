d3d5ef1a09fc19b93fce3b08d07b9d6bbaaf9be6
Juliette Mahé <mahe.juliette@outlook.com>
2022-03-25 11:09:08 +0100

    visual show cv

---
+++ src/components/candidate/CV.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import  "./CV.css";
+import file from "../../assets/files-icons/file.png"
+
+const CV = ({path, idCV}) => {
+    
+
+    return (
+        <div className='CV'>
+            <a href={`${process.env.REACT_APP_URL}${path}`} target="_blank">
+                 <div>
+                     <img className='imgCV' src={file}></img>
+                     <p>Nom du cv</p>
+                 </div>
+            </a>
+            <button>X</button>           
+        </div>
+    )
+}
+
+export default CV;
