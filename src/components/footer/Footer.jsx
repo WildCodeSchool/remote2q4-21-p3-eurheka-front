3c539474a8a1be55e7a4f5e0bf0a5591f1150a2f
Florent-Lefevre <lefevre.flo@gmail.com>
2022-04-04 11:05:04 +0200

    delete footer link marginright

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -36,16 +36,10 @@ const Footer = (props) => {
                         <NavLink to="/login">
                             <li className='navigation-list' onClick={scrollToTop}>Connexion</li>
                         </NavLink>
-                        <NavLink to="/">
-                            <li className='navigation-list' onClick={scrollToTop}>Conditions</li>
-                        </NavLink>
                     </ul>
                     <ul className='navigation-block'>
                         <NavLink to="/">
                             <li className='navigation-list' onClick={scrollToTop}>Conditions générales</li>
-                        </NavLink>
-                        <NavLink to="/">
-                            <li className='navigation-list' onClick={scrollToTop}>Conditions</li>
                         </NavLink>
                         <NavLink to="/a-propos">
                             <li className='navigation-list' onClick={scrollToTop}>A propos</li>
