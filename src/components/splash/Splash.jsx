df37901d295e7111a6cc415bfb4c9ff0ab133f10
Florent-Lefevre <lefevre.flo@gmail.com>
2022-04-07 15:34:59 +0200

    add description and responsive to splash

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -14,9 +14,15 @@ const Splash = () => {
     };
 
     return (
-        <div className={'Splash'}>
+        <div className='Splash'>
             <img src={logo_splash} alt="logo_eurheka" className='logo'/>
             <h1 className='splashTitle'>Accompagnement et transformation</h1>
+            <div className='description'>
+                <p className='paragraph'>Après une carrière toute tracée dans le monde de l'industrie et des ressources humaines, j’ai décidé de mettre à profit mon expérience et mes compétences.</p>
+                <p className='paragraph'>Vous avez des questions liées au monde du travail, vous avez besoin d’être accompagné dans votre développement ?</p>
+                <p className='paragraph'>N’hésitez-pas à échanger avec moi pour identifier votre projet d’entreprise ou vos projets individuels.</p>
+                <p className='name'>Estelle</p>
+            </div>
             <div className='buttonContainer'>
                 <button className='button' onClick={scrollToMenu}>Espace Candidat</button>
                 <button className='button'>Espace Entreprise</button>
